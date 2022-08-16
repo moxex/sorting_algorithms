e9987e6a1542a13913ce0991f03239e4bf0d386a
Moses <akojimoses@gmail.com>
2022-08-16 14:15:02 -0700

    Updated

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,77 +1,85 @@
 #include "sort.h"
+#include <stdlib.h>
 
 /**
- * return_max - returns max value in an array
+ * pow_10 - calculates a positive power of 10
+ * @power: power of 10 to calculate
  *
- * @arr: array
- * @n: size of array
- * Return: int
+ * Return: the corresponding power of 10
  */
-int return_max(int arr[], int n)
+unsigned int pow_10(unsigned int power)
 {
-	int max_val = arr[0];
-	int i;
+	unsigned int i, result;
 
-	for (i = 1; i < n; i++)
-	{
-		if (arr[i] > max_val)
-			max_val = arr[i];
-	}
-	return (max_val);
+	result = 1;
+	for (i = 0; i < power; i++)
+		result *= 10;
+	return (result);
 }
 
 /**
- * countsort - sorts an array using
- * countsort algorithm
+ * count_sort - sorts an array of integers in ascending order at a specific
+ * digit location using the Counting sort algorithm
+ * @array: array to sort
+ * @size: size of the array to sort
+ * @digit: digit to sort by
  *
- * @array: array to be sorted
- * @n: size of array
- * @place_val: place value sorting depends on
- * @output: temp sorted array
- * Returns: nothing
+ * Return: 1 if there is a need to keep sorting, 0 if not
  */
-void countsort(int *array, int n, int place_val, int *output)
+unsigned int count_sort(int *array, size_t size, unsigned int digit)
 {
-	int i;
-	int count[10] = {0};
+	int i, count[10] = {0};
+	int *copy = NULL;
+	size_t j, temp, total = 0;
+	unsigned int dp1, dp2, sort = 0;
 
-	for (i = 0; i < n; i++)
-		count[(array[i] / place_val) % 10]++;
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-	for (i = n - 1; i >= 0; i--)
+	dp2 = pow_10(digit - 1);
+	dp1 = dp2 * 10;
+	copy = malloc(sizeof(int) * size);
+	if (copy == NULL)
+		exit(1);
+	for (j = 0; j < size; j++)
 	{
-		output[count[(array[i] / place_val) % 10] - 1] = array[i];
-		count[(array[i] / place_val) % 10]--;
+		copy[j] = array[j];
+		if (array[j] / dp1 != 0)
+			sort = 1;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < 10 ; i++)
+		count[i] = 0;
+	for (j = 0; j < size; j++)
+		count[(array[j] % dp1) / dp2] += 1;
+	for (i = 0; i < 10; i++)
 	{
-		array[i] = output[i];
+		temp = count[i];
+		count[i] = total;
+		total += temp;
 	}
+	for (j = 0; j < size; j++)
+	{
+		array[count[(copy[j] % dp1) / dp2]] = copy[j];
+		count[(copy[j] % dp1) / dp2] += 1;
+	}
+	free(copy);
+	return (sort);
 }
+
 /**
- * radix_sort - sorts an array using radix
- * algorithm
- * @array: array to be sorted
- * @size: size of array
- * Returns: nothing
+ * radix_sort - sorts an array of integers in ascending order using
+ * the Radix sort algorithm
+ * @array: array to sort
+ * @size: size of the array
+ *
+ * Return: void
  */
 void radix_sort(int *array, size_t size)
 {
-	int max_val, place_val, *output;
+	unsigned int i, sort = 1;
 
-	if (array == NULL || size <= 1)
+	if (array == NULL || size < 2)
 		return;
-	output = malloc(sizeof(int) * size);
-	if (output == NULL)
+	for (i = 1; sort == 1; i++)
 	{
-		return;
-	}
-	max_val = return_max(array, size);
-	for (place_val = 1; max_val / place_val > 0; place_val *= 10)
-	{
-		countsort(array, size, place_val, output);
+		sort = count_sort(array, size, i);
 		print_array(array, size);
 	}
-	free(output);
 }
