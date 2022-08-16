003daa08a162c89b811dde8e62219a61acb509fe
Moses <akojimoses@gmail.com>
2022-08-16 13:39:29 -0700

    Fix sort

--- sort.h
+++ sort.h
@@ -1,10 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <string.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -20,19 +17,19 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_list(const listint_t *list);
-void print_array(const int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-/* QuickSort functions */
-void quicker_sort(int *array, size_t size, int lo, int hi);
-int partition(int *array, size_t size, int lo, int hi);
-
-void swap(int *ptr_a, int *ptr_b);
-void swap_ints(int *a, int *b);
-
-#endif
+#endif /* _SORT_H_ */
